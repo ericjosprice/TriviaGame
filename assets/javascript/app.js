205ca2c63d295d843ebbd1c5f5ac35eeeb43025e
ericjosprice <epriceutexas@gmail.com>
2019-03-21 21:58:38 -0500

    fixed a timer bug and updated display so that one can play on a mobile phone

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,57 @@
 var aTrivia = [{
-    q: "In our solar system, which planet has the shortest day?",
-    a: "Jupiter",
-    w: ["Earth", "Mars", "Uranus"],
-},
-{
-    q: "The Great Red Spot is a gigantic storm located on which planet in our solar system",
-    a: "Jupiter",
-    w: ["Mercury", "Venus", "Earth"],
-},
-{
-    q: "Titan, Enceladus, Mimas & Iapetus are just some of the moons orbiting which planet?",
-    a: "Jupiter",
-    w: ["Saturn", "Uranus", "Mars"],
-},
-{
-    q: "Which planet in our solar system has the most oxygen?",
-    a: "Earth",
-    w: ["Pluto", "Neptune", "Mercury"],
-},
-{
-    q: "Which planet is furthest from the sun?",
-    a: "Neptune",
-    w: ["Saturn", "Uranus", "Mercury"],
-},
-{
-    q: "What planet in our solar system has the most gravity?",
-    a: "Jupiter",
-    w: ["Saturn", "Earth", "Mercury"],
-},
+        q: "In our solar system, which planet has the shortest day?",
+        a: "Jupiter",
+        w: ["Earth", "Mars", "Uranus"],
+    },
+    {
+        q: "The Great Red Spot is a gigantic storm located on which planet in our solar system",
+        a: "Jupiter",
+        w: ["Mercury", "Venus", "Earth"],
+    },
+    {
+        q: "Titan, Enceladus, Mimas & Iapetus are just some of the moons orbiting which planet?",
+        a: "Jupiter",
+        w: ["Saturn", "Uranus", "Mars"],
+    },
+    {
+        q: "Which planet in our solar system has the most oxygen?",
+        a: "Earth",
+        w: ["Pluto", "Neptune", "Mercury"],
+    },
+    {
+        q: "Which planet is furthest from the sun?",
+        a: "Neptune",
+        w: ["Saturn", "Uranus", "Mercury"],
+    },
+    {
+        q: "What planet in our solar system has the most gravity?",
+        a: "Jupiter",
+        w: ["Saturn", "Earth", "Mercury"],
+    },
 ];
 
 var option = $(".answers")
 var question = $("#question")
 var nCounter = 0;
 var nTime = 30;
-var nConverted=30;
+var nConverted = 30;
 var nCorrect = 0;
 var nIncorrect = 0;
 var nUnanswered = 0;
 var intervalID;
 var qInterval;
-var bAnswered=false;
+var bAnswered = false;
 
 //varriables
 //=======================================================================
 $(document).ready(function () {
 
-//start
-function start(){
-    nextQuestion();
-    intervalID = setInterval(count, 1000);
-    qInterval = setInterval(nextQuestion, 30000);
-}
+    //start
+    function start() {
+        nextQuestion();
+        intervalID = setInterval(count, 1000);
+        qInterval = setInterval(nextQuestion, 30000);
+    }
     // display the question and options
 
     //question
@@ -64,35 +64,44 @@ function start(){
             nTime = 30;
 
             // change answered boolean to true
-            bAnswered= false;
+            bAnswered = false;
             //clear gif
             option.children().remove();
             $("#image-holder").empty();
             question.text(aTrivia[nCounter].q)
             // display correct option
-            option.append("<span  id='c' class='option'>" + aTrivia[nCounter].a + "</span>" + "<br>");
+            option.append("<button  id='c' class='option'>" + aTrivia[nCounter].a + "</button>");
             //wrong options
             for (var i = 0; i <= 2; i++) {
-                var newSpan = $("<button id='w'>");
-                newSpan.attr('class', 'option');
-                newSpan.html(aTrivia[nCounter].w[i] + "<br>");
-                option.append(newSpan);
+                var newButton = $("<button id='w'>");
+                var newBreak = $("<br>")
+                newButton.attr('class', 'option');
+                newButton.html(aTrivia[nCounter].w[i]);
+                option.append(newBreak);
+                option.append(newButton);
+
             }
             nCounter++;
         } else {
             clearInterval(intervalID);
             clearInterval(qInterval);
+            //display time as 0 seconds
+            nTime = 0;
+            $("#timer").empty();
+            $("#timer").text("Here is your score:");
             // option.empty();
             question.empty();
-            question.html("Correct: " +nCorrect+ "<br>" + "Incorrect: " + nIncorrect + "<br>" + "Unanswered: " + nUnanswered);
-            $(".answers").html("<button  id='again' class='option'>Play Again?</button>");   $("#image-holder").empty();   
+            question.html("Correct: " + nCorrect + "<br>" + "Incorrect: " + nIncorrect + "<br>" + "Unanswered: " + nUnanswered);
+            $(".answers").html("<button  id='again' class='option'>Play Again?</button>");
+            $("#image-holder").empty();
         }
     }
 
     function count() {
 
         nTime--;
-         nConverted = timeConverter(nTime);
+        nConverted = timeConverter(nTime);
+        $("#timer").empty();
         $("#timer").text("Time remaining: " + nConverted);
     }
 
@@ -102,8 +111,8 @@ function start(){
 
         if (seconds < 10) {
             seconds = "0" + seconds;
-        } 
-        if(t===0){
+        }
+        if (t === 0) {
             nUnanswered++;
         }
         return seconds;
@@ -111,15 +120,24 @@ function start(){
 
     //correct guess GIF
     function correct() {
-        $("#image-holder").html("<img src='assets/images/giphy.gif' width='200px'>");
-        setTimeout(nextQuestion, 6000);
+        $("#image-holder").html("Correct!! <img src='assets/images/giphy.gif' width='200px'>");
+        clearTimeout(qInterval);
+        clearInterval(intervalID);
+        setTimeout(start, 5000);
+        $("#timer").empty();
+        $("#timer").text("You got it right");
 
     }
     //incorrect guess GIFF
     function incorrect() {
+        $("#image-holder").html("Correct answer: " + aTrivia[nCounter - 1].a + "<img src='assets/images/wrong.gif' width='200px'>");
+        clearTimeout(qInterval);
+        clearInterval(intervalID);
+        setTimeout(start, 5000);
+        $("#timer").empty();
+        $("#timer").text("Wrong Answer");
 
-        $("#image-holder").html("Correct answer: "+ aTrivia[nCounter-1].a+ "<img src='assets/images/wrong.gif' width='200px'>");
-        setTimeout(nextQuestion, 6000);
+
 
     }
 
@@ -127,33 +145,33 @@ function start(){
 
 
     //main process
-//display start button
+    //display start button
     $("#timer").text("Click to start");
-    $("#timer").on("click", function(){
+    $("#timer").on("click", function () {
         $("#timer").empty()
         start();
         nextQuestion();
-})
+    })
 
     //display questions for 30 seconds each
 
     var value;
-    $(document).on("click",".option", function () {
+    $(document).on("click", ".option", function () {
         value = $(this).attr('id');
         console.log(this);
 
-        if (value === "c" && bAnswered===false) {
+        if (value === "c" && bAnswered === false) {
             nCorrect++;
-            bAnswered=true;
+            bAnswered = true;
             //play winner gif
             correct();
-        } else if (value === "w" && bAnswered===false) {
-            bAnswered=true;
+        } else if (value === "w" && bAnswered === false) {
+            bAnswered = true;
             nIncorrect++
             // value;
             // console.log(value)
             incorrect();
-        } else if(value === "again"){
+        } else if (value === "again") {
             nCounter = 0;
             nTime = 30;
             nCorrect = 0;
